f85cebc681647d2d3f7d25061a0db05ea60b1ac3
Joe <joestrong@gmail.com>
2013-09-07 15:12:51 +0100

    Cleaned up views

--- www/js/views.js
+++ www/js/views.js
@@ -1,12 +1,24 @@
+var WeatherView = Backbone.View.extend({
+
+    tagName: 'div',
+
+    initialize: function() {
+        this.render();
+    },
+
+    getWeatherHtml: function(){
+        return '<p>The weather</p>';
+    },
+
+    render: function() {
+        this.$el.html(this.getWeatherHtml());
+    }
+
+});
+
 var ClothesListView = Backbone.View.extend({
 
     tagName: 'ul',
-
-    events: {
-        "click .icon":          "open",
-        "click .button.edit":   "openEditDialog",
-        "click .button.delete": "destroy"
-    },
 
     initialize: function() {
         this.listenTo(this.collection, "add", this.render);
@@ -28,19 +40,16 @@ var AppView = Backbone.View.extend({
 
     el: $('.app'),
 
-    events: {
-        "click .icon":          "open",
-        "click .button.edit":   "openEditDialog",
-        "click .button.delete": "destroy"
-    },
-
     initialize: function() {
         this.render();
     },
 
     render: function() {
+        this.$el.html('');
+        var weatherView = new WeatherView();
         var clothesCollection = new ClothesCollection(config.clothes);
         var clotheslist = new ClothesListView({ collection: clothesCollection });
+        this.$el.append(weatherView.el);
         this.$el.append(clotheslist.el);
     }
 
